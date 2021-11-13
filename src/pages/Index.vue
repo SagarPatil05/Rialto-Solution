c09cf61e947122021272c366877e6b57de218b38
Sagar-Patil05 <sagarkachave05@gmail.com>
2021-11-13 18:02:34 +0530

    Add: website in vue demo (v1.0.0)

---
+++ src/pages/Index.vue
@@ -0,0 +1,132 @@
+<template>
+<div class="body px-5">
+  <div class="columns pt-6 pl-5">
+    <div class="column is-1">
+      <div class="">
+        <figure class="image is-48x48">
+          <img class="is-rounded" src="https://i.pinimg.com/236x/d1/b0/59/d1b059755ec1986183e8d24846626a33--lee.jpg">
+        </figure>
+    
+      </div>
+    </div>
+    <div class="column is-8">
+      <h1 class="has-text-white ">hi, Sagar Patil</h1>
+      <h1 class="has-text-grey ">how do you feel today?</h1>
+    </div>
+    <div class="column bg-color-search is-3">
+      <div class="columns">
+        <div class="column is-2">
+          <g-image class="image is-24x24" src="~/images/search.png"/>
+        </div>
+        <div class="column has-text-left">What movie you want to watch?</div>
+      </div>
+    </div>
+  </div>
+  
+  
+  <div class="columns">
+    <div class="column is-1">
+      <div class="mt-6 mr-2 bg-color">
+        <p class="home has-text-centered py-6 has-text-white">Home</p>
+        <p class="home has-text-centered py-6 has-text-white">Popular</p>
+        <p class="home has-text-centered py-6 has-text-white">TV Series</p>
+        <p class="home has-text-centered py-6 has-text-white">Movies</p>
+      </div>
+    </div>
+    <div class="column is-11 mr-6">
+      <h2 class="has-text-white title is-3 mt-6">Popular</h2>
+      <div class="columns">
+        <div class="column">
+        <g-image class="img-bg" src="~/images/avengers.jpg"/>
+        <div class="has-text-white is-size-5">Avengers</div>
+        <div class="has-text-grey is-size-6">Time : 1.30hr</div>
+      </div>
+      <div class="column">
+        <g-image class="img-bg" src="~/images/Bandish_Bandits_poster.jpg"/>
+        <div class="has-text-white is-size-5">Bandish Bandits</div>
+        <div class="has-text-grey is-size-6">Time : 1.30hr</div>
+      </div>
+      <div class="column">
+        <g-image class="img-bg" src="~/images/big bang.jpeg"/>
+        <div class="has-text-white is-size-5">big bang</div>
+        <div class="has-text-grey is-size-6">Time : 1.30hr</div>
+      </div>
+      <div class="column">
+        <g-image class="img-bg" src="~/images/black.jpeg"/>
+        <div class="has-text-white is-size-5">black widow</div>
+        <div class="has-text-grey is-size-6">Time : 1.30hr</div>
+      </div>
+      <div class="column">
+        <g-image class="img-bg" src="~/images/dehli crime.jpeg"/>
+        <div class="has-text-white is-size-5">dehli crime</div>
+        <div class="has-text-grey is-size-6">Time : 1.30hr</div>
+      </div>
+      <div class="column">
+        <g-image class="img-bg" src="~/images/freinds.jpg"/>
+        <div class="has-text-white is-size-5">freinds</div>
+        <div class="has-text-grey is-size-6">Time : 1.30hr</div>
+      </div>
+      
+      </div>
+
+      <h2 class="has-text-white title is-3 mt-6 pt-5">Category</h2>
+      <div class="columns">
+        <div class="column">
+        <g-image class="img-bg" src="~/images/The-Family-Man-1200.jpg"/>
+        <div class="has-text-white is-size-5">The Family Man</div>
+        <div class="has-text-grey is-size-6">Time : 1.30hr</div>
+      </div>
+      <div class="column">
+        <g-image class="img-bg" src="~/images/Little_Things_Poster.jpg"/>
+        <div class="has-text-white is-size-5">Little_Things_Poster</div>
+        <div class="has-text-grey is-size-6">Time : 1.30hr</div>
+      </div>
+      <div class="column">
+        <g-image class="img-bg" src="~/images/lok.jpeg"/>
+        <div class="has-text-white is-size-5">lok</div>
+        <div class="has-text-grey is-size-6">Time : 1.30hr</div>
+      </div>
+      <div class="column">
+        <g-image class="img-bg" src="~/images/money heist.jpg"/>
+        <div class="has-text-white is-size-5">money heist</div>
+        <div class="has-text-grey is-size-6">Time : 1.30hr</div>
+      </div>
+      <div class="column">
+        <g-image class="img-bg" src="~/images/ops.jpeg"/>
+        <div class="has-text-white is-size-5">ops</div>
+        <div class="has-text-grey is-size-6">Time : 1.30hr</div>
+      </div>
+      <div class="column">
+        <g-image class="img-bg" src="~/images/squid game.jpeg"/>
+        <div class="has-text-white is-size-5">squid game</div>
+        <div class="has-text-grey is-size-6">Time : 1.30hr</div>
+      </div>
+      
+      </div>
+      
+    </div>
+
+  </div>
+</div>
+</template>
+<script>
+</script>
+<style>
+.img-bg{
+  border-radius: 10px;
+  object-fit: cover;
+  height: 300px;
+  width: 180px;
+}
+.bg-color{
+  background-color: rgb(33, 33, 68);
+  border-radius: 10px;
+  margin-top: 100px;
+}
+.bg-color-search
+{
+  background-color: rgb(29, 29, 58);
+  border-radius: 10px;
+  height: 45px;
+}
+</style>
